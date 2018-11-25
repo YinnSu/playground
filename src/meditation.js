a8ab13c7e09daced9d46383fcde0fd9629e7e3c9
metalex9 <alexbainter@gmail.com>
2018-11-25 13:57:06 -0600

    Organize processed singing bowl samples

--- src/meditation.js
+++ src/meditation.js
@@ -1,31 +1,47 @@
 import Tone from 'tone';
+import { Note, Distance } from 'tonal';
 import samples from './samples.json';
 
 Tone.context.latencyHint = 'playback';
+
+const tonicPitchClasses = Note.names().slice(0, 5);
+const tonicPitchClass =
+  tonicPitchClasses[Math.floor(Math.random() * tonicPitchClasses.length)];
+
+const pitchClassesOverOctaves = (pitchClasses, octaves) =>
+  pitchClasses.reduce(
+    (notes, pitchClass) =>
+      notes.concat(octaves.map(octave => `${pitchClass}${octave}`)),
+    []
+  );
+
+const LOWER_OCTAVES = [1, 2];
+const HIGHER_OCTAVES = [3, 4];
+const lowPitchClasses = [
+  tonicPitchClass,
+  Distance.transpose(tonicPitchClass, 'P5'),
+];
+const highPitchClasses = lowPitchClasses.concat(
+  ['M2', 'P4'].map(interval => Distance.transpose(tonicPitchClass, interval))
+);
+const lows = pitchClassesOverOctaves(lowPitchClasses, LOWER_OCTAVES);
+const highs = pitchClassesOverOctaves(highPitchClasses, HIGHER_OCTAVES);
 
 const delay = new Tone.FeedbackDelay({
   wet: 0.5,
   delayTime: 20,
   feedback: 0.8,
 }).toMaster();
-const lowpass = new Tone.Filter({ rolloff: -96, frequency: 1000 }).connect(
-  delay
-);
-const glock = new Tone.Sampler(samples['kasper-singing-bowls']).connect(
-  lowpass
-);
+const glock = new Tone.Sampler(samples['kasper-singing-bowls']).connect(delay);
 
 Tone.Master.volume.value = -10;
-
-const lows = ['C1', 'G1', 'C2', 'G2'];
-const highs = ['C5', 'G5', 'C4', 'G4', 'F4', 'F5', 'D4', 'D5'];
 
 const makeNotes = notes => () => {
   glock.triggerAttack(notes[Math.floor(Math.random() * notes.length)]);
   if (Math.random() > 0.3) {
     glock.triggerAttack(
       notes[Math.floor(Math.random() * notes.length)],
-      `+${Math.random() * 10 + 1}`
+      `+${Math.random() * 15 + 1}`
     );
   }
 };
@@ -37,15 +53,15 @@ setTimeout(() => {
   lowNotes();
   setInterval(() => {
     lowNotes();
-  }, 30200);
-}, 2000);
+  }, Math.random() * 5000 + 45000);
+}, 2000 + Math.random() * 5000);
 
 setTimeout(() => {
   highNotes();
   setInterval(() => {
     highNotes();
-  }, 60230);
-}, 5000);
+  }, Math.random() * 5000 + 75000);
+}, 5000 + Math.random() * 5000);
 
 window.navigator.requestMIDIAccess().then(access => {
   console.log(access);
@@ -57,6 +73,7 @@ window.navigator.requestMIDIAccess().then(access => {
     const [command, key, value] = data;
     if (command === 144) {
       glock.triggerAttack(Tone.Midi(key).toFrequency(), '+0.1');
+      console.log(Tone.Midi(key).toNote());
     } else if (command === 176) {
       if (key === 1) {
         delay.delayTime.value = (value / 127) * 20;
