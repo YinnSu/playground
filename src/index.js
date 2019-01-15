5dbd3f7b6b3cd71c990951a977f07024d216e2e9
Alex Bainter <alexbainter@gmail.com>
2019-01-15 12:03:22 -0600

    Add new start of piece

--- src/index.js
+++ src/index.js
@@ -1,45 +1,52 @@
 import Tone from 'tone';
-import { Note } from 'tonal';
+import { Scale, Note } from 'tonal';
 import samples from './samples.json';
 
-const OCTAVES = [2, 3, 4];
-const notes = OCTAVES.reduce(
+const pianoPromise = new Promise(resolve => {
+  const piano = new Tone.Sampler(samples['vsco2-piano-mf'], {
+    onload: () => resolve(piano),
+  }).toMaster();
+});
+
+const tonic = Note.names()[Math.floor(Math.random() * Note.names().length)];
+const scalePitchClasses = Scale.notes(tonic, 'major');
+const notes = [3, 4, 5, 6, 7].reduce(
   (allNotes, octave) =>
-    allNotes.concat(Note.names().map(pitchClass => `${pitchClass}${octave}`)),
+    allNotes.concat(scalePitchClasses.map(pc => `${pc}${octave}`)),
   []
 );
 
-const getInstrument = () =>
-  new Promise(resolve => {
-    const instrument = new Tone.Sampler(samples.otherness, {
-      onload: () => resolve(instrument),
-      attack: 1,
-    });
-  });
-
-const sineSynth = new Tone.MonoSynth({
-  oscillator: { type: 'sine' },
-  envelope: {
-    attack: 3,
-    release: 10,
-  },
-}).toMaster();
-
-sineSynth.volume.value = -25;
-
-const playNote = (instrument, lastNoteMidi) => {
-  const newNotes = notes.filter(n => Note.midi(n) !== lastNoteMidi);
-  const note = newNotes[Math.floor(Math.random() * newNotes.length)];
-  instrument.triggerAttack(note, '+1.5');
-  const pitchClass = Note.pc(note);
-  sineSynth.triggerAttackRelease(`${pitchClass}1`, 5, '+1.5');
-  setTimeout(() => {
-    playNote(instrument, Note.midi(note));
-  }, Math.random() * 10000 + 10000);
+const getProgression = () => {
+  const startingIndex = Math.floor(Math.random() * notes.length);
+  const progression = [[notes[startingIndex]]];
+  const gap = Math.floor(Math.random() * 5);
+  for (let i = 2; i === 2 || i <= Math.random() * 5; i += 1) {
+    const chord = [];
+    if (i % 2 === 0) {
+      for (let j = 1; j <= i; j += 2) {
+        chord.push(startingIndex - j, startingIndex + j);
+      }
+    } else {
+      chord.push(startingIndex);
+      for (let j = 2; j <= i; j += 2) {
+        chord.push(startingIndex - j, startingIndex + j);
+      }
+    }
+    progression.push(
+      chord
+        .filter(index => index >= 0 && index < notes.length)
+        .map(index => notes[index])
+    );
+  }
+  return progression;
 };
 
-getInstrument().then(instrument => {
-  instrument.chain(Tone.Master);
-  instrument.volume.value = -5;
-  playNote(instrument);
+pianoPromise.then(piano => {
+  setInterval(() => {
+    const progression = getProgression();
+    const perNoteDelay = Math.random() * 4 + 1;
+    progression.forEach((chord, i) => {
+      chord.forEach(note => piano.triggerAttack(note, `+${i * perNoteDelay}`));
+    });
+  }, 10000);
 });
