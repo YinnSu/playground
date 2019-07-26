abb1067177b29478af4f57f723079dc4bbe1bef2
metalex9 <alexbainter@gmail.com>
2019-07-26 16:51:45 -0500

    Update

--- src/index.js
+++ src/index.js
@@ -1,70 +1,203 @@
 import Tone from 'tone';
+import { Note, Distance, Interval } from 'tonal';
 import samples from './samples.json';
 
-const getSampler = () =>
-  new Promise(resolve => {
-    const sampler = new Tone.Sampler(samples['dry-guitar-vib'], {
-      baseUrl: './samples/dry-guitar-vib/',
-      onload: () => resolve(sampler),
-    });
-  });
+const findClosest = (samplesByNote, note) => {
+  const noteMidi = Note.midi(note);
+  const maxInterval = 96;
+  let interval = 0;
+  while (interval <= maxInterval) {
+    const higherNote = Note.fromMidi(noteMidi + interval);
+    if (samplesByNote[higherNote]) {
+      return higherNote;
+    }
+    const lowerNote = Note.fromMidi(noteMidi - interval);
+    if (samplesByNote[lowerNote]) {
+      return lowerNote;
+    }
+    interval += 1;
+  }
+  return note;
+};
 
-const getBuffers = () =>
+const getBuffers = (samplesByNote, baseUrl = '') =>
   new Promise(resolve => {
-    const buffers = new Tone.Buffers(samples['vcsl-ocean-drum'], {
-      baseUrl: './samples/vcsl-ocean-drum/',
+    const buffers = new Tone.Buffers(samplesByNote, {
       onload: () => resolve(buffers),
+      baseUrl,
     });
   });
-const NOTES = ['C3', 'D#3', 'G3', 'A#3', 'C4', 'D#4', 'G4', 'A#4'];
 
-Promise.all([getSampler(), getBuffers()]).then(([guitar, oceanDrum]) => {
-  guitar.set({ curve: 'linear', attack: 10, release: 15 });
-  const reverb = new Tone.Reverb(7).set({ wet: 1 }).toMaster();
-  const delay1 = new Tone.FeedbackDelay({
-    feedback: 0.7,
-    delayTime: Math.random() * 0.2 + 0.8,
-  }).connect(reverb);
-  const delay2 = new Tone.FeedbackDelay({
-    feedback: 0.5,
-    delayTime: 0.25,
-  }).connect(delay1);
-  reverb.generate();
-  const autoFilter = new Tone.AutoFilter(Math.random() / 10, 200, 5)
-    .start()
-    .connect(delay2);
-  guitar.connect(autoFilter);
-  NOTES.forEach(note => {
-    const play = () => {
-      guitar.triggerAttack(note, '+1');
-      Tone.Transport.scheduleOnce(() => {
-        play();
-      }, `+${Math.random() * 25 + 25}`);
-    };
-    Tone.Transport.scheduleOnce(() => {
-      play();
-    }, `+${Math.random() * 25 + 10}`);
+const ragaBhairavi = [
+  [1, 2],
+  [-2, 2],
+  [0, 2],
+  [1, 2],
+  [3, 2],
+  [5, 4],
+  [7, 2],
+  [3, 2],
+  [5, 2],
+  [8, 2],
+  [10, 2],
+  [12, 4],
+  [10, 2],
+  [8, 2],
+  [7, 2],
+  [5, 2],
+  [7, 2],
+  [3, 2],
+  [5, 2],
+  [3, 2],
+  [1, 2],
+  [0, 4],
+];
+
+const ragaBhairav = [
+  [0, 2],
+  [4, 2],
+  [5, 2],
+  [7, 2],
+  [4, 2],
+  [5, 2],
+  [8, 4],
+  [11, 2],
+  [12, 4],
+  [8, 2],
+  [11, 2],
+  [8, 2],
+  [7, 2],
+  [5, 2],
+  [7, 2],
+  [4, 2],
+  [1, 2],
+  [0, 4],
+];
+
+function* makeRagaGenerator(raga) {
+  function getNewPhrase() {
+    const phrase = [];
+    let noteIdx = Math.floor(Math.random() * (raga.length - 4));
+    const remainingNoteCount = raga.length - noteIdx;
+    const maxPhraseLength = Math.ceil(Math.random() * remainingNoteCount);
+    const divisor = Math.random() * 1.25 + 0.25;
+    console.log('new phrase');
+    for (
+      let count = 0;
+      phrase.length < maxPhraseLength &&
+      noteIdx < raga.length &&
+      count < 100 &&
+      (phrase.length > 1 || Math.random() < 0.95);
+      count += 1, Math.random() < 0.7 ? (noteIdx += 1) : (noteIdx += 2)
+    ) {
+      const [interval, time] = raga[noteIdx];
+
+      const multiplier =
+        phrase.length === maxPhraseLength - 1 || noteIdx === raga.length - 1
+          ? 4
+          : 1;
+
+      phrase.push([interval, (time / divisor) * multiplier]);
+    }
+    return phrase;
+  }
+
+  let phrase;
+
+  while (true) {
+    if (
+      typeof phrase !== 'undefined' &&
+      phrase.length > 1 &&
+      Math.random() < 0.5
+    ) {
+      console.log('same phrase again');
+    } else {
+      phrase = getNewPhrase();
+    }
+    for (const note of phrase) {
+      yield note;
+    }
+  }
+}
+
+const getSampler = (samplesByNote, baseUrl, opts = {}) =>
+  new Promise(resolve => {
+    const sampler = new Tone.Sampler(
+      samplesByNote,
+      Object.assign(opts, {
+        baseUrl,
+        onload: () => resolve(sampler),
+      })
+    );
   });
 
-  const firstOceanDelays = samples['vcsl-ocean-drum'].map(
-    () => Math.random() * 30
-  );
-  const minOceanDelay = Math.min(...firstOceanDelays);
-  samples['vcsl-ocean-drum'].forEach((_, i) => {
-    const buffer = oceanDrum.get(i);
-    const play = () => {
-      buffer.reverse = Math.random() < 0.5;
-      const source = new Tone.BufferSource(buffer)
-        .set({ fadeIn: 3, fadeOut: 3, curve: 'linear', playbackRate: 0.5 })
-        .connect(reverb);
-      source.start();
-      Tone.Transport.scheduleOnce(() => {
-        play();
-      }, `+${Math.random() * buffer.duration * 2 + buffer.duration * 2 + 1}`);
-    };
+const pianoSamples = samples['vsco2-piano-mf'];
+
+Promise.all([
+  getSampler(pianoSamples, './samples/vsco2-piano-mf/'),
+  getSampler(
+    samples['vsco2-cellos-susvib-mp'],
+    './samples/vsco2-cellos-susvib-mp/',
+    { attack: 2, curve: 'linear', release: 2 }
+  ),
+  new Tone.Reverb(15)
+    .set({ wet: 0.6 })
+    .toMaster()
+    .generate(),
+]).then(([pianoSampler, cellos, reverb]) => {
+  pianoSampler.connect(reverb);
+
+  let tonic = Math.random() < 0.5 ? 'C#4' : 'C#5';
+
+  const playNote = (note, time = 0, velocity = 1) =>
+    pianoSampler.triggerAttack(note, `+${1 + time}`, velocity);
+
+  const ragaGenerator = makeRagaGenerator(ragaBhairav);
+
+  const celloFilter = new Tone.AutoFilter(
+    Math.random() / 100 + 0.01,
+    50,
+    3
+  ).connect(reverb);
+  celloFilter.start();
+  cellos.connect(celloFilter);
+
+  const celloDrone = note => {
+    cellos.triggerAttack(note, '+1');
     Tone.Transport.scheduleOnce(() => {
-      play();
-    }, `+${firstOceanDelays[i] - minOceanDelay + 1}`);
+      celloDrone(note);
+    }, `+${Math.random() * 10 + 5}`);
+  };
+
+  ['C#2', 'C#1', 'G#1', 'G#2'].forEach(note => {
+    Tone.Transport.scheduleOnce(() => {
+      celloDrone(note);
+    }, `+${Math.random() * 5}`);
   });
+
+  const playNextNote = () => {
+    const { value } = ragaGenerator.next();
+    const [interval, time] = value;
+    const note = Distance.transpose(tonic, Interval.fromSemitones(interval));
+    playNote(note);
+    if (Math.random() < (interval === 0 || interval === 12 ? 0.5 : 0.1)) {
+      const lowNote =
+        Math.random() < 0.5
+          ? 'C#3'
+          : Distance.transpose(note, Interval.fromSemitones(-12));
+      playNote(lowNote);
+    }
+    Tone.Transport.scheduleOnce(() => {
+      if (time > 8 && Math.random() < 0.4) {
+        console.log('CHANGING TONIC');
+        tonic = tonic === 'C#4' ? 'C#5' : 'C#4';
+      }
+      playNextNote();
+    }, `+${time + Math.random() - 0.5}`);
+  };
+
+  Tone.Transport.scheduleOnce(() => {
+    playNextNote();
+  }, `+${Math.random() * 2 + 2}`);
   Tone.Transport.start();
 });
