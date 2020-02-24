69844f6bd94f569a4988c691e118f584fba05169
metalex9 <alexbainter@gmail.com>
2020-02-24 17:40:06 -0600

    Update

--- src/midi-reader.js
+++ src/midi-reader.js
@@ -1,7 +1,6 @@
 window.navigator.requestMIDIAccess().then(access => {
   console.log(access);
-  const inputs = Array.from(access.inputs);
-  const device = inputs[1][1];
-  console.log(device);
-  device.onmidimessage = ({ data }) => console.log(data);
+  const inputs = Array.from(access.inputs).forEach(([name, device]) => {
+    device.onmidimessage = ({ data }) => console.log(data);
+  });
 });
